26f5eb543a7c1c7d0a7be396ea6c28227e27d578
Shivam Rana <86354964+shivamranaaa@users.noreply.github.com>
2023-09-05 16:56:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
 
 __version__ = "0.0.0"
 
@@ -18,7 +15,6 @@ setuptools.setup(
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A small python package for CNN app",
-    long_description=long_description,
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
